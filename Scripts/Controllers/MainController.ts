fdc9d83386ff02922d3eb071a6029b7dec2c1646
Andrei Rînea <andrei@rinea.ro>
2016-04-23 01:43:36 +0300

    6. Restored delete functionality

--- Scripts/Controllers/MainController.ts
+++ Scripts/Controllers/MainController.ts
@@ -40,6 +40,40 @@
                 });
         }
 
+        findContactById(id: number) {
+            var result = {
+                contact: null,
+                index: null
+            };
+
+            for (var i = 0; i < this.$scope.contacts.length; i++) {
+                if (this.$scope.contacts[i].id === id) {
+                    result.contact = this.$scope.contacts[i];
+                    result.index = i;
+                    return result;
+                }
+            }
+            return null;
+        }
+
+        delete(id: number) {
+            var result = this.findContactById(id);
+            if (!result)
+                return;
+            var consent = confirm("Are you sure you want to delete '" + result.contact.name + "' (id: " + result.contact.id + ") ?");
+            if (!consent)
+                return;
+
+            this.$scope.working = true;
+            this.$http.delete("/api/contacts" + "/" + result.contact.id).then(() => {
+                this.$scope.contacts.splice(result.index, 1);
+                this.$scope.working = false;
+            }, errorResponse => {
+                this.$scope.working = false;
+                alert("could not delete contact :( code: " + errorResponse.status);
+            });
+        }
+
         newValid() {
             return this.$scope.newContact != null && this.$scope.newContact.name != null && this.$scope.newContact.email != null;
         }
